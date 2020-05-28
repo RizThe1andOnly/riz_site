68942659a535ef0c17240e7649f2b8c64f7bc388
Rizwan Chowdhury <rizwanchowdhury111@gmail.com>
2020-05-28 17:00:13 -0400

    Updates

---
+++ precache-manifest.086e050e5992017a24cd4499b387827b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9af6ec9fbe2a48708a2255da4c26049a",
+    "url": "/riz_site/index.html"
+  },
+  {
+    "revision": "3921a03eced0fbd8bfdd",
+    "url": "/riz_site/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "104b0626c042e9695a50",
+    "url": "/riz_site/static/js/2.061f098f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/riz_site/static/js/2.061f098f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3921a03eced0fbd8bfdd",
+    "url": "/riz_site/static/js/main.26fadbf1.chunk.js"
+  },
+  {
+    "revision": "b1d09639c0040502ae71",
+    "url": "/riz_site/static/js/runtime-main.bc6279cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/riz_site/static/media/logo.5d5d9eef.svg"
+  }
+]);
